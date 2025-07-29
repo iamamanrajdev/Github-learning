0cc3394b08c2b4a71cf16c804c42f2254e9cd091
Aman Raj <iamamanraj.dev@gmail.com>
2025-07-29 22:10:33 +0530

    myfirst

---
+++ first.js
@@ -0,0 +1,51 @@
+const name = "ROhit";
+const age = 23;
+
+let balance = 420;
+
+function updateBalance(value){
+    balance+=value;
+}
+
+
+// console.log("Hello Coders");
+
+// console.log("Hello Ji");
+
+
+// const c = 20+10;
+// console.log(c);
+
+// function balancechecker(){
+//     // 
+//     console.log("UPdate");
+//     // 
+// }
+//*Problem
+//*Code update kiya website crash hogyi coder ko revert back karna hoga
+//* ownership of code matlab company me 3-4 bande ek project per kaam kar rahe to malum hona chhaiyae na kaun sa code kisne likha
+//* to comapny ne teesre bande ko baitha liya as a tracker wo track karega kaunn sa code kisne likha kya likha jaise hi ek line bhi code likhoge uske pass details chal jayega kal ko uske code ke wajah se problem hui webiste me to wo pakda jayega kiske wajah se hua hai
+//*iska faida kya hua usko pata chal jayega meri webiste itne baje sahi chal rahi thi phir wo dekhega ha itne baje codechange hua aur meri website crash kar gaye to wo samajh jyega itne baje se itne baje ke becch jisne ye code likha hai wo hatao
+//*So yahi git hai na tracker ka kaam kar rha hai
+//*man lo ek code 3 baje likha ek 4 baje ek 5 baje aur 6 baje wala code fat gaya to automatic 5baje wala code execute kar dega revert back ho jayega to git ko bolte hai version control system
+
+// 🧠 Real-life Example:
+
+// Socho tum ek assignment likh rahe ho Word file mein. Tum har 1 ghante baad uska backup bana ke alag file save karte ho:
+// assignment_v1.docx, assignment_v2.docx, assignment_final.docx
+
+// Git wahi kaam tumhare liye automatically karta hai — par programming world mein.
+
+// ⸻
+
+
+
+
+
+
+
+
+//  code likho
+//  5 member code likh rhe hai
+// update code: 
+// 3 line of code
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+🧠 Real-life Example:
+
+// Socho tum ek assignment likh rahe ho Word file mein. Tum har 1 ghante baad uska backup bana ke alag file save karte ho:
+// assignment_v1.docx, assignment_v2.docx, assignment_final.docx
+
+// Git wahi kaam tumhare liye automatically karta hai — par programming world mein.
+
+// ⸻
+
