a3137e47f27aec27e7f5c1867792b99b0e74078a
Aman Raj <iamamanraj.dev@gmail.com>
2025-08-05 15:30:58 +0530

    duplifirstcommit

--- Day03/aman.js
+++ Day03/aman.js
@@ -5,16 +5,16 @@ let year=2025;
 
 let late=230;
 
-
+//*---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //*TO JAISE HM LOG FIRSTCOMMIT PHIR SECOND COMMIT PHIR THIR COMMIT KIYA TO YE SARE KO MASTER BRANCH BOLTE HAI
 //*AUR JO LATEST COMMIT HOTA HAI USPE HEAD REHTA HAI
 //*aur ye sare branch apne parent per depend hote hai third to second se modify hoke hi na bana hoga
-
+//*------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //*1-GIT LOG -SARA DETAILS DEGA KITNA COMMIT HUA
 //* 2-GIT LOG --ONELINE SHORT ME COMMIT KA ID DETA HAI 
 //*3-GIT SHOW lateest commit me kya kya change hua hai wo dikhayega
 //*4 git show <commitid> batat hai us particula commit me kya change hua
-
+//*--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //*git reset --hard <code>
 //*Ab Manlo latest commit per head hai AUR US COMMIT KE KARAN CODE FAT GAYA TO TMKO HEAD KO REVERT BACK KARKE PURANE WALEPER LANA HAI
 //*SABSE PEHLE CHECK KARENGE HEAD KISPER HAI git log se then git reset --hard commit code to ye latest commit 
@@ -26,15 +26,28 @@ let late=230;
 
 //*AB MAN LO SECOND COMMIT EK WEBISE ME BUTTON ADD KIYE PHIR USKO COMMIT KIYE USKE KARAN CODE FAT GAYA TO RESET HARD KARNE PER TO BAKI SARA CODE DELETE KAREGA
 //*HMKO TO SIRF BUTTON WALA CODE DELETE KARNA HAI
-
+//*---------------------------------------------------------------------------------------------------------------------------------------------------
 //*Git revert
 //*ISKO ACHIEVE KARENE KE LIYE GIT REVERT USE KARENGE 
 //*git revert commit id
 //*ye bhi new commit bana ta hai jis commit se button hatana tha wo button wala code hatake baki ka code is naye commit me dalega purni wali history
 //*delete nai hoti isliye new commit per wo buton wala code hatake baki ka is new waleme dal dega
+//*--------------------------------------------------------------------------------------------------------------------------------------------
+//*Concept of BRANCHING
+//* MAN LO EK WEBISTE LIVE CHAL RHA HAI AB TMHE USPER PAYMENT GATEWAY ADD KARNA HAI
+// *WEBISTE TO MASTER BRANCH KE CODE ME COMMIT HAI USKA AB TM WAHA JAKE PAYMENT GATEWAY KA CODE LIKHOGE 4-5 DIN 
+// *TO JAYEGA HI TO MAIN BRANCH PER LIKHOGE TO WO WEBSITE PER BHI DIKHEGA ACCHA ABHI YE COMPANY
+//* PAYMENT GATEWAY ME CHANGE KAR RAHI DOOSRA CASE TM PAYMENT GATEWAY ADD KAR RAHE HO TMHARE TEAM KA DUSRA BANDA 
+// *KOI BUTTON KAR RHA PHIR TMHAE PAYMENT GATEWAY KE CODE ME KUCH ISSUE HO GAYA TO TMHE POORA CODE REVERT YA RESET KARNA PADEGA
+//* POORA MASTER BRANCH KA CODE LENGTHY HO JAYEGA
 
+//* ISLIYE MASTERBRANCH TMHE YE POWER DETA HAI EK KAM HAI TU APNA PAYMENT GATEWAY MAIN APNA BRANCH KA COPY
+//* BANA KE DE RHA HU USME APNA CODE LIKH JO BHI ERROR HAI SOLVE KAR PHIT JAB SAHI CHALEGA TO WO CODE MERE 
+//* ME MERGE KARDE .
 
-
+console.log("aman") 
+//*git checkout -b name se new branch create hoga
+//*jo bhi change karna hai delete karna hai isme hoga 
 
 
 
